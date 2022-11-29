7a25bbff2eb6f246e552177c688432498d568159
Mark Whitehead <mjamesw2010@gmail.com>
2022-11-29 21:43:43 +0000

    Added styles to form

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/AddProperty.css';
 
 function AddProperty() {
-  return <h1 className="add-property">Add Property Page</h1>;
+  const initialState = {
+    fields: {
+      title: '',
+      city: 'Manchester',
+      type: 'Flat',
+    },
+  };
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+  return (
+    <div className="add-property">
+      <h1>Add Your Property Here</h1>
+      <div>
+        <form className="add-property__form" onSubmit={handleAddProperty}>
+          <label htmlFor="title">
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+              placeholder="Name of Property"
+            />
+          </label>
+
+          <label htmlFor="city">
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="">--Please choose and option--</option>
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+            </select>
+          </label>
+
+          <label htmlFor="type">
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="">--Please Choose Option--</option>
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detatched">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End Of Terrace">End Of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AddProperty;
