0761c86957730ff79c973361f4e59279db75f784
Mark Whitehead <mjamesw2010@gmail.com>
2022-11-29 22:46:45 +0000

    Added the rest of the form inputs but not yet styled

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -5,8 +5,12 @@ function AddProperty() {
   const initialState = {
     fields: {
       title: '',
-      city: 'Manchester',
-      type: 'Flat',
+      city: 'Your City',
+      type: 'Type of Residence',
+      bedrooms: 'Number Of Bedrooms',
+      bathrooms: 'Number of bathrooms',
+      price: 0,
+      email: 'name@email.com',
     },
   };
   const [fields, setFields] = useState(initialState.fields);
@@ -34,14 +38,26 @@ function AddProperty() {
             />
           </label>
 
+          <label htmlFor="price">
+            Price :
+            <input
+              id="price"
+              name="price"
+              type="number"
+              value={fields.price}
+              onChange={handleFieldChange}
+            />
+          </label>
+
           <label htmlFor="city">
+            Your City :
             <select
               id="city"
               name="city"
               value={fields.city}
               onChange={handleFieldChange}
             >
-              <option value="">--Please choose and option--</option>
+              <option value="deafultValue">--Please choose and option--</option>
               <option value="Manchester">Manchester</option>
               <option value="Leeds">Leeds</option>
               <option value="Sheffield">Sheffield</option>
@@ -50,13 +66,14 @@ function AddProperty() {
           </label>
 
           <label htmlFor="type">
+            Type of residence:
             <select
               id="type"
               name="type"
               value={fields.type}
               onChange={handleFieldChange}
             >
-              <option value="">--Please Choose Option--</option>
+              <option value="deafultValue">--Please Choose Option--</option>
               <option value="Flat">Flat</option>
               <option value="Detached">Detached</option>
               <option value="Semi-Detatched">Semi-Detached</option>
@@ -66,6 +83,58 @@ function AddProperty() {
               <option value="Bungalow">Bungalow</option>
             </select>
           </label>
+
+          <label htmlFor="bathrooms">
+            Number of Bathrooms:
+            <select
+              id="bathrooms"
+              name="bathrooms"
+              value={fields.bathrooms}
+              onChange={handleFieldChange}
+            >
+              <option value="defaultValue">--Please Choose an Option--</option>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+            </select>
+          </label>
+
+          <label htmlFor="Bedrooms">
+            Number of Bedrooms:
+            <select
+              id="bedrooms"
+              name="bedrooms"
+              value={fields.bedrooms}
+              onChange={handleFieldChange}
+            >
+              <option value="defaultValue">--Please Choose an Option--</option>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+            </select>
+          </label>
+
+          <label htmlFor="email">
+            Your Email :
+            <input
+              type="email"
+              id="city"
+              name="city"
+              value={fields.email}
+              onChange={handleFieldChange}
+            />
+          </label>
+
           <input type="submit" value="Add" />
         </form>
       </div>
